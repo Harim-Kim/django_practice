d729b3490289641e22695eab2d3a00ffa16a1170
김하림(XW - KIM HALIM) <halimkim@koreanair.com>
2020-04-06 20:13:12 +0900

    'comeback'

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 #from bookmark.views import BookmarkDV, BookmarkLV
-
+from mysite.views import HomeView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
     #class-based views
     #path('bookmark/', BookmarkLV.as_view(), name='index'),
     #path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
+
 ]
